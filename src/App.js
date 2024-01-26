8f61ec789e7694b7d345c516b712313fc68e7198
ifra <ifra2109d01@aptechgdn.net>
2024-01-26 16:18:41 +0500

    Updated Files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home';
+import Main from './components/main';
 
 function App() {
   return (
     <div className="App">
       
 <Home/>
+<Main/>
 
     </div>
   );
---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React from    "react";
+
+
+function Main()
+{
+    return(
+        <div>
+            <h1>Some Text Here Again</h1>
+        </div>
+    )
+}
+
+
+export default Main;
