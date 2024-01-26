0ffa43d66ceedb4d2c6071045abef2a9e1c5932f
Safa <safa2109d@aptechgdn.net>
2024-01-26 15:49:43 +0500

    safa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home';
+import Work from './components/safa';
 
 function App() {
   return (
     <div className="App">
       
 <Home/>
+<Work/>
 
     </div>
   );
