ec0767c86035475085a5d732668172c1fce7d010
urba fatima <urba2112d@aptechgdn.net>
2024-01-26 15:58:03 +0500

    new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home';
+import Urba from './components/urba';
 
 function App() {
   return (
     <div className="App">
       
 <Home/>
+<Urba/>
 
     </div>
   );
