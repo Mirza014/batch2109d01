bddfe93de00b4e10256d64f7ff0a6393429b74a6
hassan2112d <hassan2112d@aptechgdn.net>
2024-01-26 15:55:00 +0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/home';
+import Hassan from './components/hassan';
+
 
 function App() {
   return (
     <div className="App">
       
 <Home/>
+<Hassan/>
 
     </div>
   );
